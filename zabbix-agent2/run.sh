a7f7f07f46cc2deea03783ef3a1b5fd403766252
Aleksandr Vasilev <alvasilev@detmir.ru>
2021-11-27 02:01:21 +0300

    -

--- zabbix-agent2/run.sh
+++ zabbix-agent2/run.sh
@@ -10,6 +10,16 @@ ZABBIX_CONFIG_FILE=/etc/zabbix/zabbix_agent2.conf
 sed -i 's@^#\?\s\?\(Server\(Active\)\?\)=.*@\1='"${ZABBIX_SERVER}"'@' "$ZABBIX_CONFIG_FILE"
 sed -i 's/^#\?\s\?\(Hostname\)=.*$/\1='"${ZABBIX_HOSTNAME}"'/' "${ZABBIX_CONFIG_FILE}"
 
+DOCKER_SOCKET=/var/run/docker.sock
+DOCKER_GROUP=docker
+REGULAR_USER=zabbix
+
+if [ -S ${DOCKER_SOCKET} ]; then
+    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
+    groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
+    usermod -aG ${DOCKER_GROUP} ${REGULAR_USER}
+fi
+
 # Run zabbix-agent2 in foreground - оригинальная команда exec su zabbix -s /bin/ash -c "zabbix_agent2 -f"
 exec su zabbix -s /bin/ash -c "zabbix_agent2 -f"
 
